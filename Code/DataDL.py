95db47b4dfd6d7f26f79e14f6fdbd0f1b850223d
AlanPolson <polson.alan@gmail.com>
2016-03-22 16:50:25 -0400

    Minor edits (spelling and so on)

--- Code/DataDL.py
+++ Code/DataDL.py
@@ -1,14 +1,32 @@
 import urllib2, cStringIO, zipfile
-def download_restful(url,pth="", zp=False):
+def download_source(url,pth="", zp=False):
     """
-    Used to download files directly from source into an adjacent 'Data' Folder.
-    pass arguments 'url' (required)
-    indicate zp = True for zip file
-    if you want to further orgaize your date within the data folder, pass pth="folder_name/" and it will be appended to the path
-    The following command should download the NY houshold puma data into a folder named 'trial' in your data folder
-    files=download_restful(url,pth="trial/",zp=True)
-
-    Returns file path for single file or list of file paths for zip archive
+    Download data from source url to an adjacent 'Data' Folder.
+    
+    Args:
+        
+        url: (required) url from where data can be downloaded. can be zip archive or a single file.
+        
+        pth: (Default="") Path of directories within Data where files should be deposited.
+        
+        zp: (Default = False) Accepts True or False. indicates if file is Zip archve or not.
+        Leave blank if you want the zipfiles to be downloaded zipped.
+    
+    Returns:
+        
+        Fie Path or List of file paths (for zip archives)
+    
+    Raises:
+        
+        'Unable to access zipfile' if url is wrong. Defaut python exeptions otherwise.
+    
+    
+    Eg:
+        The following command should download the 2013 NY Houshold PUM data 
+        and its documentation into a folder named 'trial' in your data folder.
+        
+        url = "http://www2.census.gov/acs2013_1yr/pums/csv_hny.zip"
+        files=download_source(url,pth="trial/",zp=True)
 
     """
     __author__='amp'
@@ -24,7 +42,7 @@ def download_restful(url,pth="", zp=False):
                 files.append(fil)
             return files
         except urllib2.HTTPError:
-            print "unable to open zipfile"
+            print "Unable to access zipfile"
 
     else:
         fil = path+url.split("/")[-1]
--- Data/Readme.md
+++ Data/Readme.md
@@ -1,9 +1,13 @@
-As far as possible, lets try to connect data *restfully*, to aid reproducibility.
+This Folder is intentionally left blank, to aid faster updates, and to ensure data is fetched from the source. This will aid *reproducibility* and *fast backups*.
 
-The code to read the data directly from the source can be seen [here](../Code/Restful_Data_Read.ipynb).
+To this end, we have written [code](../Code/DataDL.py) which can be utilized [in this way](../Code/Restful_Data_Read.ipynb).
 
-It downloads the data locally, in this folder.
+The code downloads the data to this data folder.
 
-The .gitignore file instructs git not to track any changes in this folder so 
-1) we all have the same path in our notebooks 
+The [.gitignore file](../'.gitignore') tells git to not update anything from the data folder.
+
+The code and the gitignore file accomplish two things:
+
+1) Reproducible work (we all have the same path in our notebooks, and the same data from the same source)
+
 2) we can 'git add --all' without worry.
