d646839fb49967c919409652bab8dbaff6cacd90
Philipp Kats <casyfill@gmail.com>
2016-04-05 22:46:46 -0400

    fixed error in ACS byState scraper

--- Code/acs/acs_block_group_scraper.py
+++ Code/acs/acs_block_group_scraper.py
@@ -3,30 +3,30 @@
 
 import requests
 import os
+import StringIO
 
-
-BASEURL = 'http://www2.census.gov/programs-surveys/acs/summary_file\
-		  /2013/data/5_year_seq_by_state/{{state}}/Tracts_Block_Groups_Only/'
+BASEURL = 'http://www2.census.gov/programs-surveys/acs/summary_file/2013/data/5_year_seq_by_state/{{state}}/Tracts_Block_Groups_Only/'
 
 states = {'NewYork': 'ny'}
 
 
 def genID(x):
     x = str(x)
-    return '0' * (7 - len(x)) + x
+    return '0' * (4 - len(x)) + x + '000'
 
 
 def download_file(url, path):
     local_filename = path + '/' + url.split('/')[-1]
 
     # NOTE the stream=True parameter
-    r = requests.get(url, stream=True)
-    with open(local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-
-    return local_filename
+    r = requests.get(url)
+    if not r.status_code == requests.codes.ok:
+    	print 'download corrupted'
+    	print r.status_code
+    else:
+	    with open(local_filename, 'wb') as f:
+			for chunk in r.iter_content(1024):
+				f.write(chunk)
 
 
 def downoadTablesForState(state, path):
@@ -46,10 +46,24 @@ def downoadTablesForState(state, path):
     	download_file(fileUrl, path)
 
 
+def downloadGeoDict(state, path):
+	'''download geofile for chosen state'''
+	global BASEURL
+
+	path += '/' + state[0]
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+	fileUrl = BASEURL.replace('{{state}}', state[0]) + 'g20135%s.csv' % state[1]
+	print fileUrl.split('/')[-1]
+	download_file(fileUrl, path)
+
+
 def main():
     for state in states.iteritems():
         print state
-        downoadTablesForState(state, '../../Data/ACS/states')
+        # downoadTablesForState(state, '../../Data/ACS/states')
+        downloadGeoDict(state, '../../Data/ACS/states')
 
 
 if __name__ == '__main__':
